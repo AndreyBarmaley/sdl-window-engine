56b728850e7b7951ccf1a5dde0fd4c6b4695e0b1
Andrey Af <public.irkutsk@gmail.com>
2019-08-13 09:39:50 +0800

    SWE version 20190812.1

---
+++ src/display_scene.cpp
@@ -0,0 +1,559 @@
+/***************************************************************************
+ *   Copyright (C) 2017 by SWE team <sdl.window.engine@gmail.com>          *
+ *                                                                         *
+ *   Part of the SWE: SDL Window Engine:                                   *
+ *   https://github.com/AndreyBarmaley/sdl-window-engine                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <algorithm>
+#include <cstring>
+
+#include "window.h"
+#include "display.h"
+#include "window_gui.h"
+#include "display_scene.h"
+
+class WindowToolTip : public WindowTexture
+{
+    Point                       mousePos;
+    u32                         mouseTimeout;
+    u32                         mouseIdle;
+    const Window*		owner;
+
+public:
+    WindowToolTip(const Texture & tx, const Window* win) : WindowTexture(tx, NULL), mouseTimeout(400), mouseIdle(0), owner(win)
+    {
+	resetState(FlagModality);
+	setState(FlagLayoutForeground);
+    }
+
+    bool isOwner(const Window* win) const { return owner == win; }
+
+protected:
+    Point fixPosition(const Point & mousepos, const Size & winsz, const Size & displaysz)
+    {
+	const int offset = 2;
+	int posx = mousepos.x + winsz.w + offset > displaysz.w ? mousepos.x - winsz.w - offset : mousepos.x + offset;
+	int posy = mousepos.y - winsz.h - offset;
+	return Point(posx, posy);
+    }
+
+    void tickEvent(u32 ms)
+    {
+        const Point & cursorPos = Display::mouseCursorPosition();
+
+        if(mousePos != cursorPos)
+        {
+            mousePos = cursorPos;
+            mouseIdle = ms;
+
+            if(isVisible())
+                setVisible(false);
+        }
+        else
+        if(! isVisible() &&
+            ms - mouseIdle > mouseTimeout)
+        {
+            setPosition(fixPosition(mousePos, size(), Display::size()));
+            setVisible(true);
+        }
+    }
+};
+
+namespace
+{
+    std::list<Window*>	sceneItems;
+    bool		sceneDirty = false;
+
+    WindowToolTip*	sceneToolTip = NULL;
+
+    Texture		cursorTexture;
+    Point		cursorOffset;
+}
+
+void DisplayScene::setCursor(const Texture & tx, const Point & off)
+{
+    cursorTexture = tx;
+    cursorOffset = off;
+
+    setDirty(true);
+}
+
+void DisplayScene::setDirty(bool f)
+{
+    sceneDirty = f;
+}
+
+bool DisplayScene::isDirty(void)
+{
+    return sceneDirty;
+}
+
+std::list<Window*> DisplayScene::findChilds(const Window & parent)
+{
+    std::list<Window*> childs;
+
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+    {
+        if(*it && (*it)->parent() == & parent) childs.push_back(*it);
+    }
+
+    return childs;
+}
+    
+void DisplayScene::addItem(Window & win)
+{
+    /* back: top level */
+    sceneItems.push_back(& win);
+}
+
+void DisplayScene::removeItem(Window & win)
+{
+    sceneItems.remove(& win);
+}
+    
+void DisplayScene::sceneDestroy(void)
+{
+    if(sceneToolTip)
+    {
+	delete sceneToolTip;
+	sceneToolTip = NULL;
+    }
+
+    if(cursorTexture.isValid())
+	cursorTexture.reset();
+
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+    {
+	(*it)->setVisible(false);
+
+	if((*it)->checkState(FlagAllocated))
+	{
+	    delete *it;
+	    *it = NULL;
+	}
+    }
+
+    sceneItems.clear();
+}
+
+void DisplayScene::sceneRedraw(void)
+{
+    // redraw only root items
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+        if(*it && (*it)->parent() == NULL) (*it)->redraw();
+
+    if(sceneToolTip) sceneToolTip->redraw();
+
+    if(cursorTexture.isValid())
+	Display::renderTexture(cursorTexture, Display::mouseCursorPosition() + cursorOffset);
+}
+
+void DisplayScene::dumpScene(void)
+{
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+        if(*it) DEBUG((*it)->toString());
+}
+
+void DisplayScene::pushEvent(const Window* dst, int code, void* data)
+{
+    SDL_Event event;
+    std::memset(&event, 0, sizeof(event));
+
+    event.type = SDL_USEREVENT;
+    event.user.code = code;
+    event.user.data1 = const_cast<Window*>(dst);
+    event.user.data2 = data;
+
+    if(0 > SDL_PushEvent(&event))
+        ERROR(SDL_GetError());
+}
+
+////////////// handle events
+enum EventFilter { KeyHandle, MouseButtonHandle, MouseTrackingHandle, SystemTickHandle };
+
+bool DisplayScene::keyDebugHandle(const SDL_KeyboardEvent & ev)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+    	    if((*it)->checkState(FlagModality))
+    	    {
+        	(*it)->keyDebugEvent(ev);
+        	return true;
+    	    }
+    	    else
+    	    // focus
+    	    if(((*it)->isFocused() || (*it)->checkState(FlagKeyHandle)) &&
+        	(*it)->keyDebugEvent(ev)) return true;
+	}
+    }
+
+    return false;
+}
+
+bool DisplayScene::keyPressHandle(int key)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+    	    if((*it)->checkState(FlagModality))
+    	    {
+        	(*it)->keyPressEvent(key);
+        	return true;
+    	    }
+    	    else
+    	    // focus
+    	    if(((*it)->isFocused() || (*it)->checkState(FlagKeyHandle)) &&
+        	(*it)->keyPressEvent(key)) return true;
+	}
+    }
+
+    return false;
+}
+
+bool DisplayScene::keyReleaseHandle(int key)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+    	    if((*it)->checkState(FlagModality))
+    	    {
+        	(*it)->keyReleaseEvent(key);
+        	return true;
+    	    }
+    	    else
+    	    // focus
+    	    if(((*it)->isFocused() || (*it)->checkState(FlagKeyHandle)) &&
+        	(*it)->keyReleaseEvent(key)) return true;
+	}
+    }
+
+    return false;
+}
+
+bool DisplayScene::textInputHandle(const std::string & str)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+    	    if((*it)->checkState(FlagModality))
+    	    {
+        	(*it)->textInputEvent(str);
+        	return true;
+    	    }
+    	    else
+    	    // focus
+    	    if(((*it)->isFocused() || (*it)->checkState(FlagKeyHandle)) &&
+        	(*it)->textInputEvent(str)) return true;
+	}
+    }
+
+    return false;
+}
+
+bool DisplayScene::mousePressHandle(const ButtonEvent & st)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+	    bool focus = (*it)->isAreaPoint(st.position());
+
+	    if(focus && ! (*it)->isFocused())
+	    {
+		(*it)->setState(FlagFocused);
+		(*it)->mouseFocusEvent();
+	    }
+	    else
+	    if(! focus && (*it)->isFocused())
+	    {
+		(*it)->resetState(FlagFocused);
+		(*it)->mouseLeaveEvent();
+	    }
+
+	    if(((*it)->isFocused() &&
+		(*it)->mousePressEvent(ButtonEvent(st.button(), st.position() - (*it)->position()))) ||
+		(*it)->checkState(FlagModality)) return true;
+	}
+    }
+
+    return false;
+}
+
+bool DisplayScene::mouseReleaseHandle(const ButtonEvent & st)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+	    bool focus = (*it)->isAreaPoint(st.position());
+
+	    if(focus && ! (*it)->isFocused())
+	    {
+		(*it)->setState(FlagFocused);
+		(*it)->mouseFocusEvent();
+	    }
+	    else
+	    if(! focus && (*it)->isFocused())
+	    {
+		(*it)->resetState(FlagFocused);
+		(*it)->mouseLeaveEvent();
+	    }
+
+	    if(((*it)->isFocused() &&
+		(*it)->mouseReleaseEvent(ButtonEvent(st.button(), st.position() - (*it)->position()))) ||
+		(*it)->checkState(FlagModality)) return true;
+	}
+    }
+
+    return false;
+}
+
+bool DisplayScene::mouseClickHandle(const ButtonsEvent & st)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+	    bool focus = (*it)->isAreaPoint(st.press().position()) && (*it)->isAreaPoint(st.release().position());
+
+	    if(focus && ! (*it)->isFocused())
+	    {
+		(*it)->setState(FlagFocused);
+		(*it)->mouseFocusEvent();
+	    }
+	    else
+	    if(! focus && (*it)->isFocused())
+	    {
+		(*it)->resetState(FlagFocused);
+		(*it)->mouseLeaveEvent();
+	    }
+
+	    if(((*it)->isFocused() &&
+		(*it)->mouseClickEvent(ButtonsEvent(st.press().button(), st.press().position() - (*it)->position(),
+                                                st.release().position() - (*it)->position()))) ||
+		(*it)->checkState(FlagModality)) return true;
+	}
+    }
+
+    return false;
+}
+
+void DisplayScene::mouseMotionHandle(const Point & pos, u32 buttons)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	{
+	    bool focus = (*it)->isAreaPoint(pos);
+
+	    if(focus && ! (*it)->isFocused())
+	    {
+		(*it)->setState(FlagFocused);
+		(*it)->mouseFocusEvent();
+	    }
+	    else
+	    if(! focus && (*it)->isFocused())
+	    {
+		(*it)->resetState(FlagFocused);
+		(*it)->mouseLeaveEvent();
+	    }
+
+	    // global mouse tracking event
+	    if((*it)->checkState(FlagMouseTracking))
+		(*it)->mouseTrackingEvent(pos, buttons);
+
+	    // local mouse motion event
+	    if(((*it)->isFocused() &&
+		(*it)->mouseMotionEvent(pos - (*it)->position(), buttons)) ||
+		(*it)->checkState(FlagModality)) break;
+	}
+    }
+
+    if(cursorTexture.isValid())
+	setDirty(true);
+}
+
+#ifdef OLDENGINE
+bool DisplayScene::userHandle(const UserEvent & ev)
+#else
+bool DisplayScene::userHandle(const SDL_UserEvent & ev)
+#endif
+{
+    // receiver exists
+    if(ev.data1)
+    {
+	if(sceneItems.end() != std::find(sceneItems.begin(), sceneItems.end(), ev.data1))
+	{
+	    Window* win = static_cast<Window*>(ev.data1);
+
+	    if(ev.code == Signal::WindowCreated)
+	    {
+		win->windowCreateEvent();
+		return true;
+	    }
+
+            return win->userEvent(ev.code, ev.data2);
+        }
+	else
+	{
+	    // skip signal, temp win 
+	    if(ev.code == WindowCreated)
+		return true;
+
+	    DEBUG("skip signal: " << String::hex(ev.code) << "win not found: " << String::hex(reinterpret_cast<std::uintptr_t>(ev.data1)));
+	    return true;
+	}
+    }
+    else
+    // broadcast: for all
+    {
+	for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+	{
+    	    if(*it && (*it)->isVisible())
+		(*it)->userEvent(ev.code, ev.data2);
+	}
+
+	return true;
+    }
+
+    return false;
+}
+
+bool DisplayScene::scrollUpHandle(const Point & pos)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible() &&
+	    (((*it)->isAreaPoint(pos) && (*it)->scrollUpEvent(pos)) ||
+	    (*it)->checkState(FlagModality))) return true;
+    }
+
+    return false;
+}
+
+bool DisplayScene::scrollDownHandle(const Point & pos)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible() &&
+	    (((*it)->isAreaPoint(pos) && (*it)->scrollDownEvent(pos)) ||
+	    (*it)->checkState(FlagModality))) return true;
+    }
+
+    return false;
+}
+
+bool DisplayScene::scrollLeftHandle(const Point & pos)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible() &&
+	    (((*it)->isAreaPoint(pos) && (*it)->scrollLeftEvent(pos)) ||
+	    (*it)->checkState(FlagModality))) return true;
+    }
+
+    return false;
+}
+
+bool DisplayScene::scrollRightHandle(const Point & pos)
+{
+    for(auto it = sceneItems.rbegin(); it != sceneItems.rend(); ++it)
+    {
+        if(*it && (*it)->isVisible() &&
+	    (((*it)->isAreaPoint(pos) && (*it)->scrollRightEvent(pos)) ||
+	    (*it)->checkState(FlagModality))) return true;
+    }
+
+    return false;
+}
+
+void DisplayScene::tickHandle(u32 ms)
+{
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+    {
+    	if(*it) (*it)->tickEvent(ms);
+	if(sceneToolTip) static_cast<Window*>(sceneToolTip)->tickEvent(ms);
+    }
+
+    auto it = std::find_if(sceneItems.rbegin(), sceneItems.rend(),
+	std::bind2nd(std::mem_fun(&Window::checkState), FlagFocused));
+
+    if(it != sceneItems.rend())
+    {
+	const Texture* tx = (*it)->tooltipTexture();
+
+	if(tx)
+	{
+	    if(sceneToolTip && ! sceneToolTip->isOwner(*it))
+	    {
+		delete sceneToolTip;
+		sceneToolTip = NULL;
+	    }
+
+	    if(! sceneToolTip)
+	    {
+		sceneToolTip = new WindowToolTip(*tx, *it);
+		sceneItems.remove(sceneToolTip);
+	    }
+	}
+	else
+	if(sceneToolTip)
+	{
+	    delete sceneToolTip;
+	    sceneToolTip = NULL;
+	}
+    }
+    else
+    if(sceneToolTip)
+    {
+	delete sceneToolTip;
+	sceneToolTip = NULL;
+    }
+}
+
+void DisplayScene::renderPresentHandle(u32 ms)
+{
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+    {
+        if(*it) (*it)->renderPresentEvent(ms);
+    }
+}
+
+void DisplayScene::displayResizeHandle(const Size & sz, bool sdlmode)
+{
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+    	    (*it)->displayResizeEvent(sz, sdlmode);
+    }
+}
+
+void DisplayScene::displayFocusHandle(bool gain)
+{
+    for(auto it = sceneItems.begin(); it != sceneItems.end(); ++it)
+    {
+        if(*it && (*it)->isVisible())
+	    (*it)->displayFocusEvent(gain);
+    }
+}
---
+++ src/display_scene.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *   Copyright (C) 2017 by SWE team <sdl.window.engine@gmail.com>          *
+ *                                                                         *
+ *   Part of the SWE: SDL Window Engine:                                   *
+ *   https://github.com/AndreyBarmaley/sdl-window-engine                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef _SWE_DISPLAY_SCENE_
+#define _SWE_DISPLAY_SCENE_
+
+#include <list>
+#include "events.h"
+
+class Window;
+class Texture;
+
+class DisplayScene
+{
+public:
+    static bool                keyDebugHandle(const SDL_KeyboardEvent &);
+    static bool                keyPressHandle(int);
+    static bool                keyReleaseHandle(int);
+    static bool                textInputHandle(const std::string &);
+    static bool                mousePressHandle(const ButtonEvent &);
+    static bool                mouseReleaseHandle(const ButtonEvent &);
+    static bool                mouseClickHandle(const ButtonsEvent &);
+    static void                mouseMotionHandle(const Point &, u32 buttons);
+#ifdef OLDENGINE
+    static bool                userHandle(const UserEvent &);
+#else
+    static bool                userHandle(const SDL_UserEvent &);
+#endif
+    static bool                scrollUpHandle(const Point &);
+    static bool                scrollDownHandle(const Point &);
+    static bool                scrollLeftHandle(const Point &);
+    static bool                scrollRightHandle(const Point &);
+    static void                tickHandle(u32);
+    static void                renderPresentHandle(u32);
+    static void                displayResizeHandle(const Size &, bool);
+    static void                displayFocusHandle(bool);
+
+public:
+    static std::list<Window*>	findChilds(const Window &);
+    static void			addItem(Window &);
+    static void			removeItem(Window &);
+    static void			dumpScene(void);
+    static void			pushEvent(const Window* dst, int code, void* data);
+
+    static void			sceneRedraw(void);
+    static void			sceneDestroy(void);
+    static void			setCursor(const Texture &, const Point &);
+
+    static bool			isDirty(void);
+    static void			setDirty(bool);
+};
+
+#endif
